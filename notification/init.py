249dfb2539ef4da01a8abfca79a8c3afabd78b4e
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-02-28 00:24:47 +0530

    Update init.py

--- notification/init.py
+++ notification/init.py
@@ -5,12 +5,10 @@ Handles all notifications and alerts
 
 from .telegram_bot import TelegramNotifier
 from .message_formatter import MessageFormatter
-from .scheduler import NotificationScheduler
 from .templates import MessageTemplates
 
 __all__ = [
     "TelegramNotifier",
     "MessageFormatter",
-    "NotificationScheduler",
-    "MessageTemplates"
+    "MessageTemplates",
 ]
